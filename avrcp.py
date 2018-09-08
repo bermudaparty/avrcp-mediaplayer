a8986adad09a49275296be1b1e78928df4e22cb7
Unknown <simon@emperor.ch>
2018-09-08 23:39:05 +0200

    second version

--- avrcp.py
+++ avrcp.py
@@ -3,14 +3,11 @@
 # Dependencies:
 # sudo apt-get install -y python-gobject
 
-import time
-import signal
 import dbus
 import dbus.service
 import dbus.mainloop.glib
 import gobject
 import Tkinter
-import sys
 
 SERVICE_NAME = "org.bluez"
 AGENT_IFACE = SERVICE_NAME + '.Agent1'
@@ -20,7 +17,9 @@ PLAYER_IFACE = SERVICE_NAME + '.MediaPlayer1'
 TRANSPORT_IFACE = SERVICE_NAME + '.MediaTransport1'
 
 STR_BOX_TITLE = "Bluetooth Audio"
-STR_WAITING = "Waiting for Media Player"
+STR_WAITING   = "Waiting for Media Player"
+STR_ART_UNKNOWN = "[Artist Unknown]"
+STR_TIT_UNKNOWN = "[Title Unknown]"
 
 
 class BluePlayer():
@@ -33,53 +32,59 @@ class BluePlayer():
     state = None
     status = None
     track = []
-    
 
-    infostring = "Test"
     window = None
-    labeltext = None
-
-
-
+    titlestring = STR_TIT_UNKNOWN
+    artiststring = STR_ART_UNKNOWN
 
     def __init__(self):
         """Specify a signal handler, and find any connected media players"""
         gobject.threads_init()
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-
         self.bus = dbus.SystemBus()
 
         self.bus.add_signal_receiver(self.playerHandler,
-                bus_name="org.bluez",
-                dbus_interface="org.freedesktop.DBus.Properties",
-                signal_name="PropertiesChanged",
-                path_keyword="path")
+                                     bus_name="org.bluez",
+                                     dbus_interface="org.freedesktop.DBus.Properties",
+                                     signal_name="PropertiesChanged",
+                                     path_keyword="path")
 
         self.findPlayer()
-        self.updateDisplay()
-        
-        self.buildInfostring()
+        self.buildTrackstrings()
+
         self.window = Tkinter.Tk()
-        self.labeltext = Tkinter.IntVar()
-        self.labeltext.set(infostring)
-        lbl = Tkinter.Label(self.window, textvariable=self.labeltext)
-        playbtn = Tkinter.Button(self.window, text="Play", command=self.play)
-        pausebtn = Tkinter.Button(self.window, text="Pause", command=self.pause)
-        
-        
-        lbl.pack()
-        playbtn.pack(side=Tkinter.LEFT)
-        pausebtn.pack(side=Tkinter.LEFT)
-        self.window.protocol("WM_DELETE_WINDOW", sys.exit)
-                
-    def refreshApp(self):
+        self.window.title(STR_BOX_TITLE)
+
+        self.titlevar = Tkinter.StringVar()
+        self.titlevar.set(self.titlestring)
+        self.artistvar = Tkinter.StringVar()
+        self.artistvar.set(self.artiststring)
+
+        emptyrow = Tkinter.Label(self.window, text="", width=20).grid(row=0, columnspan=3)
+        title = Tkinter.Label(self.window, textvariable=self.titlevar, width=20, font=("Verdana", 30, "bold")).grid(row=1, columnspan=3)
+        artist = Tkinter.Label(self.window, textvariable=self.artistvar, width=20, font=("Verdana", 18)).grid(row=2, columnspan=3)
+        emptyrow2 = Tkinter.Label(self.window, text="", width=20).grid(row=3, columnspan=3)
+        prevbtn  = Tkinter.Button(self.window, text="|<<", command=self.previous).grid(row=4, column=0, sticky="e")
+        playpausebtn = Tkinter.Button(self.window, text="Play - Pause", command=self.playpause).grid(row=4, column=1, sticky="we")
+        nextbtn  = Tkinter.Button(self.window, text=">>|", command=self.next).grid(row=4, column=2, sticky="w")
+
+        self.window.protocol("WM_DELETE_WINDOW", self.end)
+        self.updateDisplay()
+        gobject.idle_add(self.refreshWindow)
+        self.window.update()
+        self.window.minsize(self.window.winfo_width(), self.window.winfo_height())
+        self.window.maxsize(self.window.winfo_width(), self.window.winfo_height())
+
+    def refreshWindow(self):
         self.window.update()
         return True
 
+
     def start(self):
-        gobject.idle_add(self.refreshApp)
         """Start the BluePlayer by running the gobject Mainloop()"""
+        #self.window.mainloop()
+
         self.mainloop = gobject.MainLoop()
         self.mainloop.run()
 
@@ -111,7 +116,8 @@ class BluePlayer():
     def getPlayer(self, path):
         """Get a media player from a dbus path, and the associated device"""
         self.player = self.bus.get_object("org.bluez", path)
-        device_path = self.player.Get("org.bluez.MediaPlayer1", "Device", dbus_interface="org.freedesktop.DBus.Properties")
+        device_path = self.player.Get("org.bluez.MediaPlayer1", "Device",
+                                      dbus_interface="org.freedesktop.DBus.Properties")
         self.getDevice(device_path)
 
     def getDevice(self, path):
@@ -122,7 +128,7 @@ class BluePlayer():
     def playerHandler(self, interface, changed, invalidated, path):
         """Handle relevant property change signals"""
         iface = interface[interface.rfind(".") + 1:]
-#        print("Interface: {}; changed: {}".format(iface, changed))
+        #        print("Interface: {}; changed: {}".format(iface, changed))
 
         if iface == "Device1":
             if "Connected" in changed:
@@ -138,24 +144,25 @@ class BluePlayer():
                 self.updateDisplay()
             if "Status" in changed:
                 self.status = (changed["Status"])
-                
-    def buildInfostring(self):
-        global infostring
+
+    def buildTrackstrings(self):
         if self.player:
-            artist = "?"
-            title = "?"
+            artist = STR_ART_UNKNOWN
+            title = STR_TIT_UNKNOWN
             if "Artist" in self.track:
                 artist = self.track["Artist"]
             if "Title" in self.track:
                 title = self.track["Title"]
-            infostring = artist + " - " + title
+            self.artiststring = artist
+            self.titlestring = title
         else:
-            infostring = "Waiting for media player"
-                
+            self.artiststring = STR_WAITING
+            self.titlestring = "---"
+
     def updateDisplay(self):
-        self.buildInfostring()
-        print(infostring)
-        self.labeltext.set(infostring)
+        self.buildTrackstrings()
+        self.artistvar.set(self.artiststring)
+        self.titlevar.set(self.titlestring)
 
     def next(self):
         self.player.Next(dbus_interface=PLAYER_IFACE)
@@ -169,15 +176,18 @@ class BluePlayer():
     def pause(self):
         self.player.Pause(dbus_interface=PLAYER_IFACE)
 
+    def playpause(self):
+        if self.status == "playing":
+            self.pause()
+        else:
+            self.play()
+
+
 if __name__ == "__main__":
     player = None
 
     try:
         player = BluePlayer()
         player.start()
-    except KeyboardInterrupt as ex:
-        print("\nBluePlayer cancelled by user")
-    except Exception as ex:
-        print("Error: {}".format(ex))
     finally:
         if player: player.end()
